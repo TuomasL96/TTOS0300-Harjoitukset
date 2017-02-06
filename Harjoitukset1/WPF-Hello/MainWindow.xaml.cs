d3c0062f28ff2930ee241a0c0c51bbba67ab928a
k2679 <k2679@ICT-421-WS-33>
2017-02-06 13:16:29 +0200

    Initial commit

---
+++ Harjoitukset1/WPF-Hello/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Hello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int timesPressed = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            timesPressed++;
+            textBlock.Text = "Hello " + textBox.Text;
+            MessageBox.Show("Terve " + textBox.Text, "Tuomas'es Juttu");
+            textBlock1.Text = timesPressed.ToString();
+        }
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            //kutsutaan näkyviin about- ikkuna
+            About about = new About();
+            //huom ikkuna voi olla modaalinen(täytyy sulkea ennen muihin ikkonoihin palaamista) tai tavallinen
+            about.ShowDialog();
+        }
+    }
+}
