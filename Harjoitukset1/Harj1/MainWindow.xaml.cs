0d17bc6c9fa797cea3ec92a46b24df9d230ce324
k2679 <k2679@TIET-334-WS-29>
2017-02-07 15:56:07 +0200

    do 1,2

---
+++ Harjoitukset1/Harj1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harj1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int truckCount = 0;
+        int carCount = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void truckButton_Click(object sender, RoutedEventArgs e)
+        {
+            truckCount++;
+            truckBlock.Text = truckCount.ToString();
+        }
+
+        private void carButton_Click(object sender, RoutedEventArgs e)
+        {
+            carCount++;
+            carBlock.Text = carCount.ToString();
+        }
+    }
+}
---
+++ Harjoitukset1/Harj2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harj2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double input;
+        double output;
+        double convertRate = 120.1234567;
+        string value;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void toEuroButton_Click(object sender, RoutedEventArgs e)
+        {
+            double input = double.Parse(inputBox.ToString());
+            output = convertRate / input;
+            value = output.ToString("0.00");
+            outputBlock.Text = "Tulos: " + value + " euroa";
+        }
+
+        private void toMarkkaButton_Click(object sender, RoutedEventArgs e)
+        {
+            double input = double.Parse(inputBox.ToString());
+            output = convertRate * input;
+            value = output.ToString("0.00");
+            outputBlock.Text = "Tulos: " + value + " markkaa";
+        }
+    }
+}
