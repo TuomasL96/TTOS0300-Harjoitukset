8d0ae8b1276e6edff49213ada9296d2e0fefe99b
k2679 <k2679@ICT-421-WS-33>
2017-02-20 11:19:33 +0200

    do demo2

---
+++ Harjoitukset3/DataDindingDemo2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MVVMDemo;
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MVVMDemo.ViewModel.StudentViewModel svmo = new MVVMDemo.ViewModel.StudentViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            svmo.LoadStudents();
+        }
+        private void StudentViewControll_loaded(object sender, RoutedEventArgs e)
+        {
+            StudentViewControl.DataContext = svmo;
+        }
+
+        private void dgStudents_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgStudents.DataContext = svmo.Students;
+        }
+    }
+}
---
+++ Harjoitukset3/DataDindingDemo2/ViewModel/StudentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            //lisään esimerkin vuoksi muutama opiskelija, oikeassa sovelluksessa haettaisiin esim tietokannasta
+            students.Add(new Student { FirstName = "Mark", LastName = "Allen" });
+            students.Add(new Student { FirstName = "John", LastName = "Doe" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Kernell" });
+            Students = students;
+        }
+    }
+}
