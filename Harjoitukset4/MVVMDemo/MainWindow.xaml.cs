80c70f9bf225f22dd3c095f2aa7a3a0c917cb49e
k2679 <k2679@ICT-421-WS-33>
2017-02-22 14:05:42 +0200

    fix demo

---
+++ Harjoitukset4/MVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace MVVMDemo
+{
+    /// <summary>
+    /// Tehty 16.2.2017, tässä vain usercontrol
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MVVMDemo.ViewModel.StudentViewModel svmo = new ViewModel.StudentViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                svmo.LoadStudentsFromMysql();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);           
+            }
+        }
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            StudentViewControl.DataContext = svmo;
+        }
+        private void dgStudents_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgStudents.DataContext = svmo.Students;
+        }
+        private void btnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            MVVMDemo.Model.Student uusi = new Model.Student();
+            uusi.FirstName = txtFirstName.Text;
+            uusi.LastName = txtLastName.Text;
+            svmo.Students.Add(uusi);
+            txtFirstName.Text = ""; txtLastName.Text = "";
+        }
+    }
+}
+
---
+++ Harjoitukset4/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,60 @@
+﻿using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+using MySql.Data.MySqlClient;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { FirstName = "Mark", LastName = "Allen" });
+            students.Add(new Student { FirstName = "John", LastName = "Doe" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Kernell" });
+            Students = students;
+        }
+        private string GetMysqlConnectionString()
+        {
+            string pw = ""; 
+            // haetaan salasana app.config konfiguraatiotiedostosta
+            //pw = MVVMDemo.Properties.Settings.Default.Password;
+            return string.Format("Data source=mysql.labranet.jamk.fi;Initial Catalog=K2679_2;user=K2679;password={0}", pw);
+        }
+        public void LoadStudentsFromMysql()
+        {
+            try
+            {
+                ObservableCollection<Student> students = new ObservableCollection<Student>();
+                //luodaan yhteys labranetin mysql-palvelimelle
+                string connStr = GetMysqlConnectionString();
+                string sql = "SELECT firstname, lastname, asioid FROM student";
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MVVMDemo.Model.Student s = new Model.Student();
+                            s.FirstName = reader.GetString(0);
+                            s.LastName = reader.GetString(1);
+                            students.Add(s);
+                        }
+                        Students = students;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
